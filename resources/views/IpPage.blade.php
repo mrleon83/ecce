d2621bb4be10941be4b3a5dd5d1acc8c122a978b
Leon Kimpton <leon010983@yahoo.co.uk>
2022-06-25 14:25:43 +0100

    Add files via upload

--- resources/views/IpPage.blade.php
+++ resources/views/IpPage.blade.php
@@ -1,4 +1,4 @@
-<h1 style="text-align: center;">5 Day Weather Forcast for {{ $location }}</h1>
+<h1 style="text-align: center;">5 Day Weather Forcast for {{ $city }} , {{ $country }}</h1>
 
 <form method="get" action="{{ url('/') }}" style="text-align: center;">
     <input type="text" value="{{ $ip }}" name="customip" />
@@ -6,7 +6,7 @@
 </form>
 
 
-@if( $location != null )
+@if( $city != null )
 @foreach ( json_decode( $weather["weatherdata"]) as $weatherdata )
 <div style="width: 180px; float: left;border: 2px solid grey; margin: 20px; padding: 20px;">
     <img src="http://openweathermap.org/img/w/{{ $weatherdata->icon }}.png">
